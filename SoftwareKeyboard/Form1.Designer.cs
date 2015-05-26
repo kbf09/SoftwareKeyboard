bb38004da5d4307976439c6fbe360ef9b45c123a
kbf09 <pexpex123@gmail.com>
2015-05-26 13:10:47 +0900

    とりあえずキー入力までできた

--- SoftwareKeyboard/Form1.Designer.cs
+++ SoftwareKeyboard/Form1.Designer.cs
@@ -28,24 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(477, 110);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(940, 419);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(2048, 729);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -55,7 +44,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+
 
     }
 }
--- SoftwareKeyboard/Form1.cs
+++ SoftwareKeyboard/Form1.cs
@@ -16,20 +16,84 @@ namespace SoftwareKeyboard
 
         Button[] btns = new Button[50];
         string hiragana = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもや ゆ よらりるれろわ を ん";
+        Dictionary<string, string> henkan = new Dictionary<string, string>() {
+            {"あ", "a"},{"い", "i"},{"う", "u"},{"え", "e"}, {"お", "o"},
+            {"か", "ka"},{"き","ki"},{"く","ku"},{"け","ke"},{"こ","ko"},
+            {"さ","sa"},{"し","si"},{"す","su"},{"せ","se"},{"そ","so"},
+            {"た","ta"},{"ち","ti"},{"つ","tu"},{"て","te"},{"と","to"},
+            {"な","na"},{"に","ni"},{"ぬ","nu"},{"ね","ne"},{"の","no"},
+            {"は","ha"},{"ひ","hi"},{"ふ","hu"},{"へ","he"},{"ほ","ho"},
+            {"ま","ma"},{"み","mi"},{"む","mu"},{"め","me"},{"も","mo"},
+            {"や","ya"},{"  "," "},{"ゆ","yu"},{"   ",""},{"よ","yo"},
+            {"ら","ra"},{"り","ri"},{"る","ru"},{"れ","re"},{"ろ","ro"},
+            {"わ","wa"},{" "," "},{"を","wo"},{"    ",""},{"ん","nn"},
+        };
+        int size = 150;
+        int nowButtonNumber = 1;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private const int WS_EX_NOACTIVATE = 0x8000000;
+        protected override CreateParams CreateParams
         {
+            get
+            {
+                CreateParams p = base.CreateParams;
+                if (!base.DesignMode)
+                {
+                    p.ExStyle = p.ExStyle | (WS_EX_NOACTIVATE);
+                }
+                return p;
+            }
+        }
 
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Down:
+                    nowButtonNumber += 1;
+                    if (nowButtonNumber >= 50) nowButtonNumber -= 50;
+                    btns[nowButtonNumber].Select();
+                    break;
+
+                case Keys.Right:
+                    nowButtonNumber += 5;
+                    if (nowButtonNumber >= 50) nowButtonNumber -= 50;
+                    btns[nowButtonNumber].Select();
+
+                    break;
+                case Keys.Up:
+                     nowButtonNumber -= 1;
+                    if (nowButtonNumber < 0) nowButtonNumber += 50;
+                    btns[nowButtonNumber].Select();
+                    break;
+                case Keys.Left:
+                     nowButtonNumber -= 5;
+                    if (nowButtonNumber < 0) nowButtonNumber += 50;
+                    btns[nowButtonNumber].Select();
+                    break;
+
+                default:
+                    return base.ProcessDialogKey(keyData);
+                    break;
+            }
+            return true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             int i, j, sum = 0;
+
+            // とりあえずウィンドウとサイズを固定
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Size = new Size(size * 10 + 15, size * 5 + 30);
+
+            // 最前面にする
+            this.TopMost = true;
 
             for (i = 0; i < 10; i++)
             {
@@ -37,23 +101,32 @@ namespace SoftwareKeyboard
                 {
                     //Buttonクラスのインスタンスを作成する
                     btns[sum] = new System.Windows.Forms.Button();
-                    Font f = new Font("MS UI Gothic", 29);
 
                     //Buttonコントロールのプロパティを設定する
-                    btns[sum].Name = "id"+ sum;
+                    btns[sum].Name = "id."+ sum;
                     btns[sum].Text = this.hiragana.Substring(sum, 1);
-                    btns[sum].Font = f;
+                    btns[sum].Font = new Font("MS UI Gothic", 29);
                     //サイズと位置を設定する
-                    btns[sum].Location = new Point(100* i, 100 * j);
-                    btns[sum].Size = new System.Drawing.Size(100, 100);
+                    btns[sum].Location = new Point(size * i, size * j);
+                    btns[sum].Size = new System.Drawing.Size(size, size);
 
 
                     //フォームに追加する
                     this.Controls.Add(btns[sum]);
-
+                    btns[sum].Click += new EventHandler(btn_click);
                     sum++;
                 }
+
             }
+
+            // 「あ」にフォーカス
+            btns[0].Select();
+
+        }
+
+        void btn_click(object sender, EventArgs e)
+        {
+            SendKeys.Send(henkan[((Button)sender).Text]);
         }
     }
 }
